6253fa393ec913435642121c346f1b2f31afad1c
Nwankpu Ijeoma <adatimo01@gmail.com>
2023-02-07 23:58:56 +0100

    updated

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -16,19 +16,6 @@ export default {
     const { colors, message, matchColor } = pickColor();
     return { colors, message, matchColor };
 
-   // const matchColor = (value) => {
-      // do a random color based on the array index;
-      //const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      //if (colors[randomNumber] === value) {
-       // message.value = `You win... [answer: ${colors[randomNumber]}]`;
-       // return;
-      //}
-
-     // message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    //};
-
-   // return { colors, message, matchColor };
   },
 };
 </script>
